a0cc1992da6f1cbb6513a87fb86568da9e5b1f96
Fortunoxx <robert_goetz@hotmail.com>
2024-03-10 22:32:03 +0100

    change sql password on the fly

--- docker/infrastructure/startup.ps1
+++ docker/infrastructure/startup.ps1
@@ -2,6 +2,8 @@ docker-compose down
 
 Install-Module Mdbc 
 Import-Module Mdbc 
+Install-Module SqlServer
+Import-Module SqlServer 
 
 docker-compose up -d mongodb
 
@@ -12,5 +14,17 @@ Write-Host -ForegroundColor Magenta "=>" $data.value
 
 Copy-Item ./templates/prometheus/alertmanager.template.yml ./prometheus/config/alertmanager.yml
 (Get-Content ./prometheus/config/alertmanager.yml).Replace('<replace_me_discord_webhook_url>', $data.value) | Set-Content ./prometheus/config/alertmanager.yml
+
+docker-compose up -d mssql
+
+$mssql_pw = Get-MdbcData @{key="mssql.new_sa_password"}
+Write-Host "=> updating sql server password:"
+Write-Host -ForegroundColor Magenta "=>" $mssql_pw.value
+
+$pwd = "P@ssw0rd1!" # old password, we want to change that now
+$query = "USE [master]; ALTER LOGIN [sa] WITH PASSWORD=N'$($mssql_pw.value)';"
+$query
+
+Invoke-Sqlcmd -Query $query -ServerInstance "localhost,1433" -Username "sa" -Password $pwd -TrustServerCertificate
 
 docker-compose up -d 
