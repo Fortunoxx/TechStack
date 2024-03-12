2bd2487171d4c3ded3a47e73964ed47e9d608e55
Fortunoxx <robert_goetz@hotmail.com>
2024-03-12 21:35:55 +0100

    update packages

--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -4,9 +4,9 @@ namespace TechStack.Domain.Entities;
 
 public class User : BaseAuditableEntity
 {
-    public string Email { get; init; }
-    public string FirstName { get; init; }
-    public string LastName { get; init; }
-    public string Mobile { get; init; }
-    public string Phone { get; init; }
+    public string? Email { get; init; }
+    public string? FirstName { get; init; }
+    public string? LastName { get; init; }
+    public string? Mobile { get; init; }
+    public string? Phone { get; init; }
 }
