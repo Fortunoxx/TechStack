5486000511c285d52d4a2c9e227f8f4b57a5b4a1
Fortunoxx <robert_goetz@hotmail.com>
2024-03-18 21:57:12 +0100

    added a sqlproj

---
+++ src/Database/DatabaseProjectStackOverflow2010/PostTypes.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[PostTypes] (
+    [Id]   INT           IDENTITY (1, 1) NOT NULL,
+    [Type] NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_PostTypes__Id] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+
---
+++ src/Database/DatabaseProjectStackOverflow2010/VoteTypes.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[VoteTypes] (
+    [Id]   INT          IDENTITY (1, 1) NOT NULL,
+    [Name] VARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_VoteType__Id] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+
