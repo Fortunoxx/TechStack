1a7a660625ef20705986b2b7211f975e005bb3f0
Fortunoxx <robert_goetz@hotmail.com>
2024-03-23 21:40:55 +0100

    wip

--- test/Application.UnitTests/UserQueryConsumerUnitTests.cs
+++ test/Application.UnitTests/UserQueryConsumerUnitTests.cs
@@ -5,6 +5,7 @@ using FluentAssertions;
 using MassTransit;
 using MassTransit.Testing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.Extensions.DependencyInjection;
 using NSubstitute;
 using TechStack.Application.Common.Interfaces;
@@ -15,7 +16,6 @@ using TechStack.Domain.Entities;
 [Trait("Category", "UnitTest")]
 public class UserQueryConsumerUnitTests
 {
-
     [Fact]
     public async Task GetAllUsersQueryConsumer_GetAllUsers_ShouldSuceedAsync()
     {
@@ -47,5 +47,40 @@ public class UserQueryConsumerUnitTests
         act.Should().NotBeNull();
         (await testHarness.Consumed.Any<GetAllUsersQuery>()).Should().BeTrue();
         (await testHarness.Sent.Any<GetAllUsersQueryResult>()).Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task GetUserByIdQueryConsumer1_GetUser_ShouldSuceedAsync()
+    {
+        // Arrange
+        var dbContext = Substitute.For<IApplicationDbContext>();
+        var dbSetUser = Substitute.For<DbSet<User>, IQueryable<User>, IAsyncEnumerable<User>>();
+        var users = new Fixture().CreateMany<User>();
+        dbSetUser.AsNoTracking().Returns(users.AsQueryable());
+        dbContext.Users.Returns(dbSetUser);
+
+        await using var provider = new ServiceCollection()
+            .AddMassTransitTestHarness(x =>
+            {
+                x.AddConsumer<GetUserByIdQueryConsumer>();
+            })
+            .AddTransient(_ => dbContext)
+            .AddAutoMapper(cfg => cfg.AddProfile<UserMappingProfile>())
+            .BuildServiceProvider(true);
+
+        var testHarness = provider.GetTestHarness();
+
+        await testHarness.Start();
+
+        var cut = testHarness.GetRequestClient<GetUserByIdQuery>();
+        var query = new Fixture().Create<GetUserByIdQuery>();
+
+        // Act
+        var act = await cut.GetResponse<GetUserByIdQueryResult>(query);
+
+        // Assert
+        act.Should().NotBeNull();
+        (await testHarness.Consumed.Any<GetAllUsersQuery>()).Should().BeTrue();
+        (await testHarness.Sent.Any<GetUserByIdQueryResult>()).Should().BeTrue();
     }
 }
