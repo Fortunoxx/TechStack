ee4237128164947deb0fb2e3afc8386717779b97
Fortunoxx <robert_goetz@hotmail.com>
2024-03-23 20:56:47 +0100

    UnitTests

--- test/Application.UnitTests/UserCommandConsumerUnitTests.cs
+++ test/Application.UnitTests/UserCommandConsumerUnitTests.cs
@@ -21,8 +21,6 @@ public class UserCommandConsumerUnitTests
     {
         // Arrange
         var dbContext = Substitute.For<IApplicationDbContext>();
-        var fakeUsers = Substitute.For<DbSet<User>>();
-        dbContext.Users.Returns(fakeUsers);
         dbContext.SaveChangesAsync(default).ReturnsForAnyArgs(1);
 
         await using var provider = new ServiceCollection()
@@ -39,10 +37,10 @@ public class UserCommandConsumerUnitTests
         await testHarness.Start();
 
         var cut = testHarness.GetRequestClient<AddUserCommand>();
-        var query = new Fixture().Create<AddUserCommand>();
+        var command = new Fixture().Create<AddUserCommand>();
 
         // Act
-        var act = await cut.GetResponse<AddUserCommandResponse>(query);
+        var act = await cut.GetResponse<AddUserCommandResponse>(command);
 
         // Assert
         act.Should().NotBeNull();
@@ -55,8 +53,6 @@ public class UserCommandConsumerUnitTests
     {
         // Arrange
         var dbContext = Substitute.For<IApplicationDbContext>();
-        var fakeUsers = Substitute.For<DbSet<User>>();
-        dbContext.Users.Returns(fakeUsers);
 
         await using var provider = new ServiceCollection()
             .AddMassTransitTestHarness(x =>
@@ -72,14 +68,76 @@ public class UserCommandConsumerUnitTests
         await testHarness.Start();
 
         var cut = testHarness.GetRequestClient<AddUserCommand>();
-        var query = new Fixture().Create<AddUserCommand>();
+        var command = new Fixture().Create<AddUserCommand>();
 
         // Act
-        var act = await cut.GetResponse<FaultedResponse>(query);
+        var act = await cut.GetResponse<FaultedResponse>(command);
 
         // Assert
         act.Should().NotBeNull();
         (await testHarness.Consumed.Any<AddUserCommand>()).Should().BeTrue();
+        (await testHarness.Sent.Any<FaultedResponse>()).Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task DeleteUserCommandConsumer_RemoveUser_ShouldSucceedAsync()
+    {
+        // Arrange
+        var dbContext = Substitute.For<IApplicationDbContext>();
+        var user = new Fixture().Create<User>();
+        dbContext.Users.FindAsync(default).ReturnsForAnyArgs(user);
+
+        await using var provider = new ServiceCollection()
+            .AddMassTransitTestHarness(x =>
+            {
+                x.AddConsumer<DeleteUserCommandConsumer>();
+            })
+            .AddTransient(_ => dbContext)
+            .BuildServiceProvider(true);
+
+        var testHarness = provider.GetTestHarness();
+
+        await testHarness.Start();
+
+        var cut = testHarness.GetRequestClient<DeleteUserCommand>();
+        var query = new Fixture().Create<DeleteUserCommand>();
+
+        // Act
+        var act = await cut.GetResponse<AcceptedResponse>(query);
+
+        // Assert
+        act.Should().NotBeNull();
+        (await testHarness.Consumed.Any<DeleteUserCommand>()).Should().BeTrue();
+        (await testHarness.Sent.Any<AcceptedResponse>()).Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task DeleteUserCommandConsumer_RemoveUserFallback_ShouldReturnFaultedResponsedAsync()
+    {
+        // Arrange
+        var dbContext = Substitute.For<IApplicationDbContext>();
+
+        await using var provider = new ServiceCollection()
+            .AddMassTransitTestHarness(x =>
+            {
+                x.AddConsumer<DeleteUserCommandConsumer>();
+            })
+            .AddTransient(_ => dbContext)
+            .BuildServiceProvider(true);
+
+        var testHarness = provider.GetTestHarness();
+
+        await testHarness.Start();
+
+        var cut = testHarness.GetRequestClient<DeleteUserCommand>();
+        var command = new Fixture().Create<DeleteUserCommand>();
+
+        // Act
+        var act = await cut.GetResponse<FaultedResponse>(command);
+
+        // Assert
+        act.Should().NotBeNull();
+        (await testHarness.Consumed.Any<DeleteUserCommand>()).Should().BeTrue();
         (await testHarness.Sent.Any<FaultedResponse>()).Should().BeTrue();
     }
 }
