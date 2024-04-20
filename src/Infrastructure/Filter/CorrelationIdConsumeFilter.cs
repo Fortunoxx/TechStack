5ea33b80b1151697701b49fa9c98f42bed727a00
Fortunoxx <robert_goetz@hotmail.com>
2024-04-20 23:14:30 +0200

    split files

--- src/Infrastructure/Filter/CorrelationIdConsumeFilter.cs
+++ src/Infrastructure/Filter/CorrelationIdConsumeFilter.cs
@@ -3,35 +3,11 @@ namespace TechStack.Infrastructure.Filter;
 using System.Threading.Tasks;
 using MassTransit;
 using Microsoft.Extensions.Logging;
-using TechStack.Application.Common.Interfaces;
 
-public class CorrelationIdPublishFilter<T> : IFilter<PublishContext<T>>
+public class CorrelationIdConsumeFilter<T>(ILogger<CorrelationIdConsumeFilter<T>> logger) : IFilter<ConsumeContext<T>>
     where T : class
 {
-    private readonly ICorrelationIdGenerator correlationIdGenerator;
-
-    public CorrelationIdPublishFilter(ICorrelationIdGenerator correlationIdGenerator)
-        => this.correlationIdGenerator = correlationIdGenerator;
-
-    public Task Send(PublishContext<T> context, IPipe<PublishContext<T>> next)
-    {
-        context.Headers.Set("X-Correlation-Id", correlationIdGenerator.Get());
-
-        return next.Send(context);
-    }
-
-    public void Probe(ProbeContext context)
-    {
-    }
-}
-
-public class CorrelationIdConsumeFilter<T> : IFilter<ConsumeContext<T>>
-    where T : class
-{
-    private readonly ILogger<CorrelationIdConsumeFilter<T>> logger;
-
-    public CorrelationIdConsumeFilter(ILogger<CorrelationIdConsumeFilter<T>> logger)
-        => this.logger = logger;
+    private readonly ILogger<CorrelationIdConsumeFilter<T>> logger = logger;
 
     public void Probe(ProbeContext context) { }
 
