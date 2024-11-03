4c307688937464befdac322308951a6237364753
Fortunoxx <robert_goetz@hotmail.com>
2024-11-03 19:48:34 +0100

    script optimizations

--- docker/infrastructure/startup.ps1
+++ docker/infrastructure/startup.ps1
@@ -5,13 +5,19 @@ $highlight_color = "Magenta"
 Write-Host "=> Stopping docker compose project..." -ForegroundColor $info_color
 docker compose down
 
-Install-Module Mdbc 
+if (-not (Get-Module -ListAvailable -Name Mdbc)) {
+    Install-Module Mdbc -Force
+}
+if (-not (Get-Module -ListAvailable -Name SqlServer)) {
+    Install-Module SqlServer -Force
+}
 Import-Module Mdbc 
-Install-Module SqlServer
 Import-Module SqlServer 
 
 Write-Host "=> Starting mongodb to get configurations..." -ForegroundColor $info_color
 docker compose up -d mongodb
+
+Connect-Mdbc . docker-compose config
 
 Write-Host "=> Setting up prometheus alertmanager..." -ForegroundColor $info_color
 Connect-Mdbc . docker-compose config
@@ -20,7 +26,9 @@ Write-Host "=> updating discord webhook url:" -ForegroundColor $info_color
 Write-Host "=>" $data.value -ForegroundColor $highlight_color 
 
 Copy-Item ./templates/prometheus/alertmanager.template.yml ./prometheus/config/alertmanager.yml
-(Get-Content ./prometheus/config/alertmanager.yml).Replace('<replace_me_discord_webhook_url>', $data.value) | Set-Content ./prometheus/config/alertmanager.yml
+$content = Get-Content ./prometheus/config/alertmanager.yml
+$content = $content -replace '<replace_me_discord_webhook_url>', $data.value
+Set-Content ./prometheus/config/alertmanager.yml -Value $content
 
 Write-Host "=> Starting mssql to apply credentials..." -ForegroundColor $info_color
 docker compose up -d mssql
@@ -28,43 +36,53 @@ docker compose up -d mssql
 # Wait for SQL Server migration steps, otherwise login will fail
 $sleeper = 0.0
 $sleeper_increment = 0.33
+$retry_count = 0
+
 DO {
     $sleeper += $sleeper_increment
     $line = docker container logs mssql | Where-Object { $_ -like "*Service Broker manager has started.*" }
     # $running = docker inspect -f '{{.State.Running}}' mssql
     Write-Host "=> Waiting for SQL Server migration steps ($sleeper s)" -ForegroundColor $warning_color
-    sleep $sleeper_increment
+    Start-Sleep $sleeper_increment
+    $retry_count++
 } Until ($line -or $sleeper -ge 15.0)
 
-$mssql_pw_ini_str = (docker inspect -f '{{.Config.Env}}' mssql).Split(" ") | Where-Object { $_ -like "*MSSQL_SA_PASSWORD*" } | Select -First 1
-$mssql_pw_ini = $mssql_pw_ini_str.Split("=")[1] # old password, we want to change that later
-$mssql_pw = (Get-MdbcData @{key = "mssql.new_sa_password" }).value # new and secure password
+$mssql_running = docker inspect -f '{{.State.Running}}' mssql
+if ($mssql_running -eq $false) {
+    Write-Host "=> MSSQL container is not running. Starting MSSQL container..." -ForegroundColor $warning_color
+    docker compose up -d mssql
+    Start-Sleep -Seconds 10
+}
 
-$new_user = "TechStackUser"
-Write-Host "=> creating $new_user..." -ForegroundColor $info_color
+$msSqlPasswordIniString = (docker inspect -f '{{.Config.Env}}' mssql).Split(" ") | Where-Object { $_ -like "*MSSQL_SA_PASSWORD*" } | Select -First 1
+$msSqlPasswordIni = $msSqlPasswordIniString.Split("=")[1] # old password, we want to change that later
+$msSqlPassword = (Get-MdbcData @{key = "mssql.new_sa_password" }).value # new and secure password
+
+$newUser = "TechStackUser"
+Write-Host "=> creating $newUser..." -ForegroundColor $info_color
 
 $query = "USE [master];
 GO
-CREATE LOGIN [$($new_user)] WITH PASSWORD=N'$($mssql_pw)', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=OFF, CHECK_POLICY=ON;
+CREATE LOGIN [$($newUser)] WITH PASSWORD=N'$($msSqlPassword)', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=OFF, CHECK_POLICY=ON;
 GO
-ALTER SERVER ROLE [sysadmin] ADD MEMBER [$($new_user)];
+ALTER SERVER ROLE [sysadmin] ADD MEMBER [$($newUser)];
 GO";
 
 Invoke-Sqlcmd -Query $query `
     -ServerInstance "localhost,1433" `
     -Username "sa" `
-    -Password $mssql_pw_ini `
+    -Password $msSqlPasswordIni `
     -TrustServerCertificate
-
+    
 Write-Host "=> updating sa password:" -ForegroundColor $info_color
-Write-Host "=>" $mssql_pw -ForegroundColor $highlight_color
-
-$query = "USE [master]; ALTER LOGIN [sa] WITH PASSWORD=N'$($mssql_pw)';"
-
+Write-Host "=>" $msSqlPassword -ForegroundColor $highlight_color
+    
+$query = "USE [master]; ALTER LOGIN [sa] WITH PASSWORD=N'$($msSqlPassword)', CHECK_EXPIRATION=OFF, CHECK_POLICY=ON;"
+    
 Invoke-Sqlcmd -Query $query `
     -ServerInstance "localhost,1433" `
-    -Username $new_user `
-    -Password $mssql_pw `
+    -Username $newUser `
+    -Password $msSqlPassword `
     -TrustServerCertificate
 
 # create volumes if needed
