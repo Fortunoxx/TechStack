30180f14d93adac468c4b5c4221b8a012f0d7eb4
Fortunoxx <robert_goetz@hotmail.com>
2024-11-20 23:22:04 +0100

    used decorated AutoMapper with custom functionality

--- test/TechStack.Application.UnitTests/MappingUnitTests.cs
+++ test/TechStack.Application.UnitTests/MappingUnitTests.cs
@@ -1,15 +1,18 @@
 namespace TechStack.Application.UnitTests;
 
+using System.Linq;
+using System.Linq.Expressions;
 using AutoFixture;
 using AutoMapper;
 using AutoMapper.Internal;
 using FluentAssertions;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using TechStack.Application.Common.Mappings;
 using TechStack.Application.Users.Commands;
 using TechStack.Application.Users.Queries;
 using TechStack.Domain.Entities;
 
-public class AutoMapperUnitTestFixtureException(string message, IEnumerable<(Type TSource, Type TDestination)> untestedMappings) : Exception(message)
+public class UnitTestAutoMapperException(string message, IEnumerable<(Type TSource, Type TDestination)> untestedMappings) : Exception(message)
 {
     public override string Message
     {
@@ -19,8 +22,7 @@ public class AutoMapperUnitTestFixtureException(string message, IEnumerable<(Typ
             {
                 var message = base.Message;
                 message += $"{Environment.NewLine}==================";
-                message += $"{Environment.NewLine}Found untested mappings. Please add tests for these mappings and verify";
-                message += $"{Environment.NewLine}that you used {nameof(AutoMapperUnitTestFixture.AddTestedMapping)} to add them to the list of tested mappings.";
+                message += $"{Environment.NewLine}Found untested mappings. Please add unit tests for these mappings and make sure to use {nameof(UnitTestAutoMapper)}";
                 message += $"{Environment.NewLine}==================";
                 message += $"{Environment.NewLine}Untested Mappings:";
                 message += $"{Environment.NewLine}==================";
@@ -34,16 +36,21 @@ public class AutoMapperUnitTestFixtureException(string message, IEnumerable<(Typ
     }
 }
 
-public class AutoMapperUnitTestFixture
+public class UnitTestAutoMapper(IMapper mapper) : IMapper
 {
-    public HashSet<(Type Source, Type Destination)> TestedMappings { get; init; } = [];
+    public static HashSet<(Type Source, Type Destination)> TestedMappings { get; } = [];
 
-    public void AddTestedMapping<TSrc, TDest>()
+    public static void AddTestedMapping<TSrc, TDest>()
     {
         TestedMappings.Add((typeof(TSrc), typeof(TDest)));
     }
 
-    public void AssertUnitTestCoverage(IMapper mapper)
+    public static void AddTestedMapping<TDest>(object source)
+    {
+        TestedMappings.Add((source.GetType(), typeof(TDest)));
+    }
+
+    public static void AssertUnitTestCoverage(IMapper mapper)
     {
         var allMappings = mapper.ConfigurationProvider.Internal().GetAllTypeMaps();
         var untestedMappings = allMappings
@@ -52,28 +59,93 @@ public class AutoMapperUnitTestFixture
 
         if (untestedMappings.Any())
         {
-            throw new AutoMapperUnitTestFixtureException("All mappings must be tested.", untestedMappings);
+            throw new UnitTestAutoMapperException("All mappings must be tested.", untestedMappings);
         }
+    }
+
+    public IConfigurationProvider ConfigurationProvider => mapper.ConfigurationProvider;
+
+    public TDestination Map<TDestination>(object source, Action<IMappingOperationOptions<object, TDestination>> opts)
+    {
+        return mapper.Map(source, opts);
+    }
+
+    public TDestination Map<TSource, TDestination>(TSource source, Action<IMappingOperationOptions<TSource, TDestination>> opts)
+    {
+        return mapper.Map(source, opts);
+    }
+
+    public TDestination Map<TSource, TDestination>(TSource source, TDestination destination, Action<IMappingOperationOptions<TSource, TDestination>> opts)
+    {
+        return mapper.Map(source, destination, opts);
+    }
+
+    public object Map(object source, Type sourceType, Type destinationType, Action<IMappingOperationOptions<object, object>> opts)
+    {
+        return mapper.Map(source, sourceType, destinationType, opts);
+    }
+
+    public object Map(object source, object destination, Type sourceType, Type destinationType, Action<IMappingOperationOptions<object, object>> opts)
+    {
+        return mapper.Map(source, destination, sourceType, destinationType, opts);
+    }
+
+    public TDestination Map<TDestination>(object source)
+    {
+        AddTestedMapping<TDestination>(source);
+        return mapper.Map<TDestination>(source);
+    }
+
+    public TDestination Map<TSource, TDestination>(TSource source)
+    {
+        return mapper.Map<TSource, TDestination>(source);
+    }
+
+    public TDestination Map<TSource, TDestination>(TSource source, TDestination destination)
+    {
+        return mapper.Map(source, destination);
+    }
+
+    public object Map(object source, Type sourceType, Type destinationType)
+    {
+        return mapper.Map(source, sourceType, destinationType);
+    }
+
+    public object Map(object source, object destination, Type sourceType, Type destinationType)
+    {
+        return mapper.Map(source, destination, sourceType, destinationType);
+    }
+
+    public IQueryable<TDestination> ProjectTo<TDestination>(IQueryable source, object parameters = null, params Expression<Func<TDestination, object>>[] membersToExpand)
+    {
+        return mapper.ProjectTo(source, parameters, membersToExpand);
+    }
+
+    public IQueryable<TDestination> ProjectTo<TDestination>(IQueryable source, IDictionary<string, object> parameters, params string[] membersToExpand)
+    {
+        return mapper.ProjectTo<TDestination>(source, parameters, membersToExpand);
+    }
+
+    public IQueryable ProjectTo(IQueryable source, Type destinationType, IDictionary<string, object> parameters = null, params string[] membersToExpand)
+    {
+        return mapper.ProjectTo(source, destinationType, parameters, membersToExpand);
     }
 }
 
 [Trait("Category", "UnitTest")]
 public class MappingUnitTests
-    : IClassFixture<AutoMapperUnitTestFixture>
 {
-    private readonly AutoMapperUnitTestFixture autoMapperUnitTestFixture;
-    private readonly IMapper mapper;
+    private readonly UnitTestAutoMapper mapper;
 
-    public MappingUnitTests(AutoMapperUnitTestFixture autoMapperUnitTestFixture)
+    public MappingUnitTests()
     {
-        this.autoMapperUnitTestFixture = autoMapperUnitTestFixture;
-
         var config = new MapperConfiguration(cfg => cfg.AddProfile<UserMapping>());
-        mapper = config.CreateMapper();
+        var mapper = config.CreateMapper();
+        this.mapper = new UnitTestAutoMapper(mapper);
     }
 
     [Fact]
-    public void AllMappingsShouldBeTested() => autoMapperUnitTestFixture.AssertUnitTestCoverage(mapper);
+    public void AllMappingsShouldBeTested() => UnitTestAutoMapper.AssertUnitTestCoverage(mapper);
 
     [Fact]
     public void Application_Mapping_ShouldWorkAsync()
@@ -104,7 +176,6 @@ public class MappingUnitTests
 
         // Act
         var result = mapper.Map<GetUserByIdQueryResult>(user);
-        // autoMapperUnitTestFixture.AddTestedMapping<User, GetUserByIdQueryResult>();
 
         // Assert
         result.Should().BeEquivalentTo(user, cfg => cfg.ExcludingMissingMembers());
@@ -118,7 +189,6 @@ public class MappingUnitTests
 
         // Act
         var result = mapper.Map<User>(getUserByIdQueryResult);
-        autoMapperUnitTestFixture.AddTestedMapping<GetUserByIdQueryResult, User>();
 
         // Assert
         result.Should().BeEquivalentTo(getUserByIdQueryResult);
@@ -132,7 +202,6 @@ public class MappingUnitTests
 
         // Act
         var result = mapper.Map<User>(addUserCommand);
-        autoMapperUnitTestFixture.AddTestedMapping<AddUserCommand, User>();
 
         // Assert
         result.Should().BeEquivalentTo(addUserCommand);
