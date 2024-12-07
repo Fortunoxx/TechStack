00adec4dce1ea228a9ce2e0cc16ec8e4b8936332
Fortunoxx <robert_goetz@hotmail.com>
2024-12-07 17:45:13 +0100

    migrated to .net 9

--- test/TechStack.Web.IntegrationTests/Users/UsersControllerIntegrationTests.cs
+++ test/TechStack.Web.IntegrationTests/Users/UsersControllerIntegrationTests.cs
@@ -13,6 +13,7 @@ using TechStack.Web.IntegrationTests.Fixtures;
 using Xunit;
 using TechStack.Infrastructure.Data;
 
+[Trait("Category", "IntegrationTest")]
 public sealed class UsersControllerIntegrationTests : IAsyncLifetime,
     IClassFixture<IntegrationTestFactory<Program, ApplicationDbContext>>
 {
