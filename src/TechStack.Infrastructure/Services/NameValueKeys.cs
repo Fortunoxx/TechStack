ab91ce3188608b27fdf52403340f57c2da78fd21
Fortunoxx <robert_goetz@hotmail.com>
2025-01-26 21:49:33 +0100

    used HashSet

--- src/TechStack.Infrastructure/Services/NameValueKeys.cs
+++ src/TechStack.Infrastructure/Services/NameValueKeys.cs
@@ -2,8 +2,9 @@ namespace TechStack.Infrastructure.Services;
 
 public static class NameValueKeys
 {
-    public static readonly IReadOnlyList<string> Keys =
-    [
+    // Define the keys that are expected to be returned by the fetchers, HashSet is used to ensure uniqueness
+    public static readonly IReadOnlyCollection<string> Keys = new HashSet<string>
+    {
         "Default", "First", "Middle", "Last"
-    ];
-}
+    };
+}   
