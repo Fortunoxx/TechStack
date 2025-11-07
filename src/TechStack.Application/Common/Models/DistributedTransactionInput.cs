a49da9068b4d50a52dd7e68901e7e80006fb78d5
Fortunoxx <robert_goetz@hotmail.com>
2025-11-07 22:01:18 +0100

    ExcludeFromCodeCoverage

--- src/TechStack.Application/Common/Models/DistributedTransactionInput.cs
+++ src/TechStack.Application/Common/Models/DistributedTransactionInput.cs
@@ -1,9 +1,15 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace TechStack.Application.Common.Models;
 
+[ExcludeFromCodeCoverage]
 public record DistributedTransactionInput(string Key);
 
+[ExcludeFromCodeCoverage]
 public record DistributedTransactionOutput(Guid? Id);
 
+[ExcludeFromCodeCoverage]
 public record DistributedTransactionCommand(string Key, string CorrelationId);
 
+[ExcludeFromCodeCoverage]
 public record DistributedTransactionResponse(Guid? Id);
--- src/TechStack.Application/Common/Models/FaultedResponse.cs
+++ src/TechStack.Application/Common/Models/FaultedResponse.cs
@@ -1,5 +1,7 @@
 namespace TechStack.Application.Common.Models;
 
+using System.Diagnostics.CodeAnalysis;
 using System.Net;
 
+[ExcludeFromCodeCoverage]
 public record FaultedResponse(HttpStatusCode HttpStatusCode, object Payload);
