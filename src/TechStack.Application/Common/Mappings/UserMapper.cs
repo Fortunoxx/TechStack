d0023712b6048c2596b26feeebe837adfc0a9558
Fortunoxx <robert_goetz@hotmail.com>
2026-07-13 00:21:58 +0200

    feat: added mapping attributes

--- src/TechStack.Application/Common/Mappings/UserMapper.cs
+++ src/TechStack.Application/Common/Mappings/UserMapper.cs
@@ -8,12 +8,21 @@ using TechStack.Domain.Entities;
 [Mapper]
 public partial class UserMapper
 {
+    [MapperIgnoreSource(nameof(User.DomainEvents))]
+    [MapperIgnoreSource(nameof(User.MetaData))]
     [MapProperty(nameof(User.AboutMe), nameof(GetUserByIdQueryResult.AboutMe))]
     public partial GetUserByIdQueryResult UserToGetUserByIdQueryResult(User user);
 
+    [MapperIgnoreTarget(nameof(User.MetaData))]
+    [MapperIgnoreTarget(nameof(User.Created))]
+    [MapperIgnoreTarget(nameof(User.CreatedBy))]
+    [MapperIgnoreTarget(nameof(User.LastModified))]
+    [MapperIgnoreTarget(nameof(User.LastModifiedBy))]
+    [MapperIgnoreTarget(nameof(User.Id))]
     [MapProperty(nameof(AddUserCommand.AboutMe), nameof(User.AboutMe))]
     public partial User AddUserCommandToUser(AddUserCommand command);
 
+    [MapperIgnoreTarget(nameof(User.MetaData))]
     [MapProperty(nameof(GetUserByIdQueryResult.AboutMe), nameof(User.AboutMe))]
     public partial User GetUserByIdQueryResultToUser(GetUserByIdQueryResult queryResult);
 }
